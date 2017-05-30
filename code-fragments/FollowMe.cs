5fda5c226f7be19ee9bcf66f9369e629aa575b7e
phil <pva@philvanallen.com>
2017-05-30 16:31:06 -0700

    first created

---
+++ code-fragments/FollowMe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MovementType {
+    MoveTowards,
+    Lerp
+};
+
+public class FollowMe : MonoBehaviour {
+
+    public float speed = 1;
+    public Vector3 offset = new Vector3(0, 0, 0);
+    public MovementType movementType;
+
+    void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        var headPosition = Camera.main.transform.position;
+        switch(movementType) {
+            case MovementType.MoveTowards:
+                transform.position = Vector3.MoveTowards(transform.position, headPosition + offset, step);
+                break;
+            case MovementType.Lerp:
+                transform.position = Vector3.Lerp(transform.position, headPosition + offset, step);
+                break;
+        }
+    }
+}
